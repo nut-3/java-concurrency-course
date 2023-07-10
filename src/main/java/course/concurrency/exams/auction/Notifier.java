50ed801081b84802a884bc64556ad201925ae97a
Konstantin Shurukhin <kshurukhin@gmail.com>
2023-07-10 19:51:08 +0300

    3-7 1.1 run notifications asynchronously

--- src/main/java/course/concurrency/exams/auction/Notifier.java
+++ src/main/java/course/concurrency/exams/auction/Notifier.java
@@ -1,16 +1,29 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Notifier {
 
+    private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
     public void sendOutdatedMessage(Bid bid) {
-        imitateSending();
+        executor.submit(this::imitateSending);
     }
 
     private void imitateSending() {
         try {
             Thread.sleep(2000);
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+        }
     }
 
-    public void shutdown() {}
+    public void shutdown() {
+        try {
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException ignored) {
+        }
+        executor.shutdownNow();
+    }
 }
