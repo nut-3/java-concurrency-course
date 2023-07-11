1cb39c51983e3107e031bf4ed5c940ffc42751b8
Konstantin Shurukhin <kshurukhin@gmail.com>
2023-07-11 13:28:14 +0300

    3-7 1.1 optimize Notifier

--- src/main/java/course/concurrency/exams/auction/Notifier.java
+++ src/main/java/course/concurrency/exams/auction/Notifier.java
@@ -2,10 +2,23 @@ package course.concurrency.exams.auction;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Notifier {
 
-    private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private static final ThreadFactory THREAD_FACTORY = Executors.defaultThreadFactory();
+    private final ExecutorService executor;
+
+    public Notifier() {
+        this.executor = Executors.newFixedThreadPool(1_000, r -> {
+            Thread thread = THREAD_FACTORY.newThread(r);
+            thread.setPriority(Thread.MIN_PRIORITY);
+            return thread;
+        });
+        ((ThreadPoolExecutor) this.executor).prestartAllCoreThreads();
+    }
+
 
     public void sendOutdatedMessage(Bid bid) {
         executor.submit(this::imitateSending);
@@ -19,6 +32,7 @@ public class Notifier {
     }
 
     public void shutdown() {
-        executor.shutdownNow();
+        executor.shutdown();
+        ((ThreadPoolExecutor) executor).getQueue().clear();
     }
 }
