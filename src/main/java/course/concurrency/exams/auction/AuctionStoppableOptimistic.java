ecfd7c8233ae9e1ec4345bf61ea9cf589727fb26
Konstantin Shurukhin <kshurukhin@gmail.com>
2023-07-12 09:26:53 +0300

    3-7 1.2 implement stoppable auctions

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -1,29 +1,47 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AuctionStoppableOptimistic implements AuctionStoppable {
 
     private Notifier notifier;
+    private AtomicReference<Bid> latestBid = new AtomicReference<>(new Bid(-1L, null, -1L));
+    private volatile boolean stopped = false;
 
     public AuctionStoppableOptimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
+        if (stopped) {
+            return false;
         }
-        return false;
+        Bid prev;
+        do {
+            prev = latestBid.get();
+            if (stopped || bid.getPrice() <= prev.getPrice()) {
+                return false;
+            }
+        } while (!trySet(prev, bid));
+        return true;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.get();
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        stopped = true;
+        notifier.shutdown();
+        return latestBid.get();
+    }
+
+    private boolean trySet(Bid prev, Bid bid) {
+        boolean result = latestBid.compareAndSet(prev, bid);
+        if (result) {
+            notifier.sendOutdatedMessage(prev);
+        }
+        return result;
     }
 }
--- src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
@@ -2,18 +2,22 @@ package course.concurrency.exams.auction;
 
 public class AuctionStoppablePessimistic implements AuctionStoppable {
 
+    private final Object lock = new Object();
+    private volatile Bid latestBid = new Bid(-1L, null, -1L);
+    private volatile boolean stopped = false;
     private Notifier notifier;
 
     public AuctionStoppablePessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
+        if (stopped) {
+            return false;
+        }
+        if (bid.getPrice() > latestBid.getPrice() && updateBid(bid)) {
             notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
             return true;
         }
         return false;
@@ -24,6 +28,20 @@ public class AuctionStoppablePessimistic implements AuctionStoppable {
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        synchronized (lock) {
+            stopped = true;
+            notifier.shutdown();
+            return latestBid;
+        }
+    }
+
+    private boolean updateBid(Bid bid) {
+        synchronized (lock) {
+            if (!stopped && bid.getPrice() > latestBid.getPrice()) {
+                latestBid = bid;
+                return true;
+            }
+            return false;
+        }
     }
 }
